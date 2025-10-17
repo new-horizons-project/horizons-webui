886650e1dc8079acf32c09a3f1e9b5a2e065d1c5
at-elcapitan <gamforceman1808@gmail.com>
2025-10-18 00:21:47 +0300

    add: mobile support

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -101,7 +101,9 @@ const login = () => {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: center;
 	width: 50%;
+	max-width: 600px;
 	height: max-content;
 	min-height: 400px;
 	gap: 20px;
@@ -194,21 +196,6 @@ const login = () => {
 	animation: shake 0.3s ease;
 }
 
-@keyframes shake {
-	0%, 100% {
-		transform: translateX(0);
-	}
-	25% {
-		transform: translateX(-5px);
-	}
-	50% {
-		transform: translateX(5px);
-	}
-	75% {
-		transform: translateX(-5px);
-	}
-}
-
 input {
 	background: transparent;
 	border: none;
@@ -256,6 +243,14 @@ button:active {
 	color: rgb(200, 200, 200);
 }
 
+@media (max-width: 1000px) {
+	.form-background-block {
+		width: 90%;
+		max-height: 555px;
+		height: 80%;
+	}
+}
+
 @keyframes appear {
 	to {
 		opacity: 1;
@@ -280,6 +275,21 @@ button:active {
 @keyframes spin {
 	to {
 		transform: rotate(360deg);
+	}
+}
+
+@keyframes shake {
+	0%, 100% {
+		transform: translateX(0);
+	}
+	25% {
+		transform: translateX(-5px);
+	}
+	50% {
+		transform: translateX(5px);
+	}
+	75% {
+		transform: translateX(-5px);
 	}
 }
 </style>
